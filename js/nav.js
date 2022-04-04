5db3e46add8547c1e1e16be9e590002eb9df7726
jbelliard <junioralex199203@gmail.com>
2022-04-04 10:46:44 -0400

    hero slider

---
+++ js/nav.js
@@ -0,0 +1,36 @@
+document.write(<nav class="site-menu-nav">
+      <div class="logo">logo</div>
+      <ul class="site-menu">
+   
+        <input type="checkbox" id="checkbox_toggle" />
+        <label for="checkbox_toggle" class="hamburger">&#9776;</label>
+     
+        <div class="menu">
+          <li class="site-menu-item">
+            <a href="./web/contenido.html">About</a>
+          </li>
+          <li class="services">
+            <a href="./web/Experience.html">Experience</a>
+      
+            <ul class="dropdown">
+              <li><a href="/">Spa </a></li>
+              <li><a href="/">Fun</a></li>
+              <li><a href="/">Water</a></li>
+              <li><a href="/">Trip</a></li>
+              <li><a href="/">Fun</a></li>
+            </ul>
+          </li>
+          <li class="site-menu-item">
+            <a href="./web/gallery.html">Gallery</a>
+          </li>
+          <li class="site-menu-item">
+            <a href="./web/residences.html">Residences</a>
+          </li>
+
+          <li class="site-menu-item">
+            <a href="./web/contact.html">Contact</a>
+          </li>
+        </div>
+      </ul>
+    </nav>
+);
